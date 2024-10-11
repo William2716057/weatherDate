8ccf902d189aa30774fa5eb176773f4b8b0af641
William2716057 <77903649+William2716057@users.noreply.github.com>
2024-10-11 19:53:10 +1300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,46 @@
 
+document.getElementById('weatherForm').addEventListener('submit', async function (e) {
+    e.preventDefault();
+
+    const location = document.getElementById('location').value;
+    const date = document.getElementById('date').value;
+    const time = document.getElementById('time').value;
+    const dateTime = `${date} ${time}`;
+
+    const weatherData = await getWeather(location, dateTime);
+    displayWeather(weatherData);
+});
+
+async function getWeather(location, dateTime) {
+    const apiKey = 'YOUR_API_KEY'; 
+    const url = `https://api.weatherapi.com/v1/history.json?key=${apiKey}&q=${location}&dt=${dateTime.split(' ')[0]}`;
+
+    const response = await fetch(url);
+    if (!response.ok) {
+        throw new Error(`Error: ${response.statusText}`);
+    }
+    const data = await response.json();
+
+
+    const hours = dateTime.split(' ')[1].split(':')[0];
+    const historicalData = data.forecast.forecastday[0].hour.find(hour => hour.time.split(' ')[1].split(':')[0] == hours);
+
+    return historicalData;
+}
+
+function displayWeather(data) {
+    const weatherResult = document.getElementById('weatherResult');
+
+    if (!data) {
+        weatherResult.innerHTML = `<p>Error: Weather data not found</p>`;
+        return;
+    }
+
+    const time = data.time.split(' ')[1];
+    const temp = data.temp_c;
+    const condition = data.condition.text;
+
+    weatherResult.innerHTML = `
+        <p>Weather at ${time}: ${temp}°C, ${condition}</p>
+    `;
+}
